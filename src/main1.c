16b3a49dcb4ecfeac798b5bbf62c89fea45e02f3
StellaDaniele <stella.dani1@gmail.com>
2022-10-19 21:31:02 +0200

    fixed ov7670 test

--- src/main1.c
+++ src/main1.c
@@ -2,12 +2,13 @@
 
 #include <avr/io.h>
 #include <util/delay.h>
+#include <stdbool.h>
 
-#include "arduino_init.hpp"
-#include "camera_setup.hpp"
-#include "capture.hpp"
-#include "macros_regs_ov7670.hpp"
-#include "utilities.hpp"
+#include "arduino_init.h"
+#include "camera_setup.h"
+#include "capture.h"
+#include "macros_regs_ov7670.h"
+#include "utilities.h"
 
 #define WIDTH 320
 #define HEIGHT 240
