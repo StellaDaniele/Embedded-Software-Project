88b34ada588a1611b629c894b4e3c7dc85f4461f
StellaDaniele <stella.dani1@gmail.com>
2022-10-20 21:48:05 +0200

    fix last test

--- src/main4.c
+++ src/main4.c
@@ -11,8 +11,8 @@
 
 int main(void) {
     start_lcd();
-    uint8_t temperature = 10;
-    uint8_t humidity = 10;
+    uint8_t temperature = 0;
+    uint8_t humidity = 0;
     int res;
     while (1) {
         res = read(&temperature, &humidity, NULL);
@@ -20,9 +20,15 @@ int main(void) {
             disp_num(1, 1, res);
             error_led();
         }
+        //disp_str_num(1,1,"TEMP: ", (int)temperature);
+        //disp_str_num(1,2,"HUMI: ", (int)humidity);
 
-        disp_str_num(1,1,"TEMP: ", (int)temperature);
-        disp_str_num(1,2,"HUMI: ", (int)humidity);
+        disp_str(1,1,"TEMP: ");
+        disp_num(7,1, (int)temperature);
+        disp_str(9,1,"C");
+        disp_str(1,2,"HUMI: ");
+        disp_num(7,2, (int)humidity);
+        disp_str(9,2,"%");
         // DHT11 sampling rate is 1HZ.
         _delay_ms(1000);
     }
