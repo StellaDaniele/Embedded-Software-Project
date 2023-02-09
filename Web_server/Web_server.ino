1a2a3257f91e17f8afff6c99173374a427d082c2
StellaDaniele <stella.dani1@gmail.com>
2023-02-09 23:42:38 +0100

    kind of works, performance issues

--- Web_server/Web_server.ino
+++ Web_server/Web_server.ino
@@ -2,7 +2,7 @@
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
-#include <SoftwareSerial.h>
+#include <stdlib.h>
 
 #include "ssid_password.hpp"
 
@@ -12,148 +12,94 @@
 const char* ssid = SSID;
 const char* password = PASSWORD;
 const int led = D1;
+char* bf;
+char* rgb_buf;
 
-char command[] = { '*', 'R', 'D', 'Y', '*' };
-
+const char command[] = { '*', 'R', 'D', 'Y', '*' };
 WiFiServer server(80);
-volatile static int rgb;
-
-SoftwareSerial ser(D1, D2);  // RX, TX
 
 void setup(void) {
+  bf = new char[10];
+  rgb_buf = new char[10];
   ESP.wdtEnable(16000);
-  ESP.wdtDisable();
-  // pinMode(led, OUTPUT);
-  // digitalWrite(led, 0);
-  Serial.begin(115200);
-  WiFi.begin(ssid, password);
-  Serial.println("");
+  // ESP.wdtDisable();
 
+  Serial.begin(1000000);
+  WiFi.begin(ssid, password);
+#ifdef DEBUG
+  Serial.println("");
+#endif
   // Wait for connection
   while (WiFi.status() != WL_CONNECTED) {
     ESP.wdtFeed();
     delay(500);
+#ifdef DEBUG
     Serial.print(".");
+#endif
   }
+#ifdef DEBUG
   Serial.println("");
   Serial.print("Connected to ");
   Serial.println(ssid);
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
-
+#endif
 
   server.begin();
+
+#ifdef DEBUG
   Serial.println("HTTP server started");
-
-  ser.begin(1000000);
+#endif
 }
-
-int t1, t2, t3;
 
 void loop(void) {
   ESP.wdtFeed();
   WiFiClient client = server.available();
   if (client) {
+#ifdef DEBUG
     Serial.println("New client detected.");
-    String currentLine = "";
-    client.print("<!DOCTYPE html><html><style>table, th, td {border-collapse: collapse;} td {width: 1px; height: 1px;}</style><body>");
+#endif
+    String buffer = "";
+    client.print("<!DOCTYPE html><html><head><title>IOT_webserver</title> \
+      <meta name='viewport' content='width=device-width, initial-scale=1.0'>\
+      <style>table, th, td {border-collapse: collapse;} td {width: 1px; height: 1px;}\
+      </style></head><body><table style=\"width: 320px; height: 240px\">");
+    int i = 0;
+    int x = 0;
+    int y = 0;
+    client.print("<tr>");
     while (1) {
       ESP.wdtFeed();
-      while (ser.available()){
-        int tmp = ser.read();
-        client.println(tmp);
-      }
-      
-    }
-    /*
-    while (1) {
-      ESP.wdtFeed();
-      int i = 0;
-      while (true) {
+      while (Serial.available()) {
         ESP.wdtFeed();
-        // Wait until we receive *RDY*, which marks the start
-        // of an image
-        int valid = false;
-        i = 0;
-
-        while (true) {
-          ESP.wdtFeed();
-          if (ser.available()) {
-            char cc = (char)ser.read();
-
-            if (command[i] == cc) {
-              i++;
-            } else {
-              i = 0;
-            }
-            if (i >= sizeof(command)) {
-              break;
-            }
-          }
-        }
-
-        // Clear read buffer
-        while (ser.available()) {
-          ESP.wdtFeed();
-          ser.read();
-        }
-
-        int tmp = 0;
-
-        // Read the image - just a list of bytes, column by column
-        // Each byte represents the brightness 0 - 255
-
-        for (int y = 0; y < HEIGHT; y++) {
-          for (int x = 0; x < WIDTH; x++) {
-            tmp = (int)ser.read();
-            int r = tmp & 0xFF;
-            int g = tmp & 0xFF;
-            int b = tmp & 0xFF;
-            client.println(r);
+        int tmp = Serial.read();
+        // client.print(tmp);
+        // client.print(" ");
+        i++;
+        if (i < 5)
+          continue;
+        if (y < HEIGHT) {
+          if (x < WIDTH) {
+            int rgb = tmp & 0xFF;
+            rgb_buf = itoa(rgb, bf, 10);
+            //client.println(r);
+            buffer += "<td style=\"background-color:rgb(";
+            buffer += rgb_buf;
+            buffer += ",";
+            buffer += rgb_buf;
+            buffer += ",";
+            buffer += rgb_buf;
+            buffer += ")\"></td>";
+            client.print(buffer);
+            buffer = "";
+            x++;
+          } else {
+            x = 0;
+            y++;
+            client.print("</tr>");
           }
         }
       }
     }
-*/
-    /*
-    client.print("<table style=\"width: 320px; height: 240px\">");
-    //int buffer = 0;
-
-    for (int x = 0; x < HEIGHT; x++) {
-      client.print("<tr>");
-      for (int y = 0; y < WIDTH; y++) {
-        ////////// TODO!!
-        /////////file >> buffer;
-        // doesn't really matter, just use serial afterwards
-        while (Serial.available() <= 3)
-          ;
-        // read the incoming byte:
-        t1 = Serial.read();
-        t2 = Serial.read();
-        if(t2==' ')
-        t3 = Serial.read();
-        int _ = Serial.read();
-        t1 -= '0';
-        t2 -= '0';
-        t3 -= '0';
-        rgb = t1 * 100 + t2 * 10 + t3;
-        Serial.print("I received: ");
-        Serial.println(rgb);
-
-        client.print("<td style=\"background-color:rgb(");
-        client.print(rgb);
-        client.print(",");
-        client.print(rgb);
-        client.print(",");
-        client.print(rgb);
-        client.print(")\">");
-        client.print("</td>");
-      }
-      client.print("</tr>");
-    }
-    client.print("</table>\n</body>\n</html>\n");
-    */
-    client.println();
-    client.stop();
   }
 }
